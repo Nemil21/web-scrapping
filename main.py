cfbf8586926133e99b4952505fa254af33b90236
Nemil21 <nemilshah212005@gmail.com>
2023-10-29 11:49:42 +0530

    Web-scraping Encode PyData

---
+++ main.py
@@ -0,0 +1,22 @@
+from bs4 import BeautifulSoup
+with open ('index.html', 'r') as html_file:
+    content = html_file.read()
+    #print(content)
+
+    soup = BeautifulSoup(content, 'lxml')
+    #print(soup.prettify())
+    event_tags = soup.find_all('h5')
+    #print(event_tags )
+    # for event in event_tags:
+    #     print(event.text)
+    event_cards = soup.find_all('div', class_ = 'card')
+    for event in event_cards:
+        # print(event.a)
+        event_name = event.h5.text
+        # print(event_name)
+
+        event_prize = event.a.text
+        # print(event_prize)
+        print(f'{event_name} has {event_prize}')
+
+    
